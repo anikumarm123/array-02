c372c1237fad3ae0aeb01ea5e325397ccb3d07a9
muthukumar <anikumar2825@gmail.com>
2022-06-14 15:27:18 +0530

    addfile.

---
+++ script.js
@@ -0,0 +1,32 @@
+// indexOf() //
+//  let pets = ["dog","cat","rat","rabit"];
+//  console.log(pets.indexOf("rat"));
+
+
+// join() //
+// let s = ["Well","Come","to","Fabevy"];
+// console.log(s.join(" "));
+// console.log(s.join("-"));
+
+// keys() //
+// si_no = [1,2,3,4,5];
+
+// key = si_no.keys();
+
+// for (const roll of key) {
+//     console.log(roll);
+// }
+
+// LastIndexOf() //
+// let pets = ["dog","cat","rat","fish","rat"];
+// console.log(pets.lastIndexOf("rat"));
+
+// pop() //
+// let  pets = ["dog","cat","rat","fish"];
+// console.log(pets.pop());
+// console.log(pets);
+
+// push() //
+// let  pets = ["dog","cat","rat"];
+// console.log(pets.push("fish"));
+// console.log(pets);
